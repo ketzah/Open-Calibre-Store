cc1379146ecfc93ce276b4c871e4dd4c9384de26
ketzah <53633920+ketzah@users.noreply.github.com>
2026-08-10 20:03:45 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ class OpenCalibreStore(StoreBase):
 
     author = "ketzah"
 
-    version = (1, 2, 1)
+    version = (1, 2, 3)
 
     actual_plugin = "{}.store:OpenCalibreStore".format(__name__)
 
